fc90abd44f77e9a360c1cb806c3d18520fc97c8e
Arvin Behshad <contact_arvin@me.com>
2015-10-16 23:47:25 +0200

    initial commit

---
+++ src/LinkedList/Node.java
@@ -0,0 +1,18 @@
+package LinkedList;
+
+/**
+ * 
+ * @author arvin
+ *
+ * @param <T>
+ *            Generic type
+ */
+public class Node<T> {
+
+	private T head;
+	private Node<T> tail;
+
+	public Node(T head, Node<T> rest) {
+
+	}
+}
