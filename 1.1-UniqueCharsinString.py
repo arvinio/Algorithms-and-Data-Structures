5c5a0d809908dea105b8dd70484f27d1fbe3ea4e
Arvin Behshad <contact_arvin@me.com>
2015-10-17 10:16:49 +0200

    refactored Quicksort

--- 1.1-UniqueCharsinString.py
+++ 1.1-UniqueCharsinString.py
@@ -4,41 +4,10 @@ Problem: Find out if a string contains unique characters. Refrain from using add
 Approach: Sort char array s, then compare s[i] with s[i+1]
 """
 
-#params point out array index
-def quicksort(s, lo, hi):
-	if lo < hi:
-		pivot = partition(s, lo, hi)
-		quicksort(s, lo, (pivot - 1))
-		quicksort(s, (pivot + 1), hi)
-	return s
-
-def partition(a, lo, hi):
-	leftwall = lo
-	pivot = a[lo]
-
-	for i in range(lo + 1, hi + 1):
-		if a[i] < pivot:
-			leftwall = leftwall + 1
-			swap(a, leftwall, i)
-
-	swap(a, lo, leftwall)
-	return leftwall
-
-def swap(s, a, b):
-	temp = s[a]
-	s[a] = s[b]
-	s[b] = temp
-
-def do_quicksort(s):
-	if type(s) is str:
-		s = list(s)
-	res = quicksort(s, 0, (len(s)-1))
-	res = ''.join(res)
-
-	return res
+import Quicksort as sort
 
 def ifUniqueLetters(s):
-	s = do_quicksort(s)
+	s = sort.do_quicksort(s)
 
 	isUnique = True
 	for i in range(0, len(s)-1):
--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -1,5 +1,38 @@
 package linkedList;
 
-public class LinkedList {
+public class LinkedList<T extends Comparable<T>> {
+	private Node<T> first;
+	// TODO keep track of last node
 
+	public static void main(String[] args) {
+		LinkedList<Character> word = new LinkedList<Character>();
+	}
+
+	public void insertAt(int index, Node<T> list, Node<T> newNode) {
+		if (index < 0 || index - 1 > getLength()) {
+			System.out.println("Index out of bounds.");
+			return;
+		}
+		if (index == 0) {
+			Node<T> node0 = this.first;
+			Node<T> node1 = node0.getTail();
+			Node<T> node2 = node1.getTail();
+
+			this.first = node1;
+			node1.setTail(node0);
+			node0.setTail(node2);
+		}
+	}
+
+	public int getLength() {
+		return getLengthHelper(this.first);
+	}
+
+	private int getLengthHelper(Node<T> node) {
+		if (node == null) {
+			return 0;
+		} else {
+			return 1 + getLengthHelper(node.getTail());
+		}
+	}
 }
