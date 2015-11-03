5f69ab0609cfbea7943a5be15bb2906062bb95c0
Arvin Behshad <contact_arvin@me.com>
2015-11-03 16:03:10 +0100

    inital commit

---
+++ src/strings/StringCompression.java
@@ -0,0 +1,39 @@
+package strings;
+
+/**
+ * 
+ * @author arvin
+ * Problem: Create a basic string compression alg using counts of the
+ * repeated chars. If the compressed string wouldn't be shorter, return the
+ * original string.
+ * 
+ * Sample input: aabcccccaaa
+ * Sample output: a2b1c5a3
+ * 
+ * Steps:
+ * -convert to char[]
+ * -traverse array
+ * --get char
+ * --while same char
+ * ---count char freq
+ * --if diff char
+ * ---save count, then reset
+ * 
+ * 
+ */
+
+public class StringCompression {
+
+	public static void main(String[] args) {
+		String input = "aabcccccaaa";
+		char[] indata = input.toCharArray();
+
+		char[] outdata;
+
+		for (int i = 0; i < : indata) {
+			
+		}
+
+	}
+
+}
