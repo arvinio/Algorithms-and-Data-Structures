92fa16aebb14ae6680bd3aa7d13a3df76cfbba9b
Arvin Behshad <contact_arvin@me.com>
2015-12-02 00:02:53 +0100

    created basic BST node class

--- src/binarySearchTree/Node.java
+++ src/binarySearchTree/Node.java
@@ -1,5 +1,16 @@
 package binarySearchTree;
 
 public class Node {
+	private int key;
+	private String value;
+
+	public Node(int Key, String value) {
+		this.key = key;
+		this.value = value;
+	}
+
+	public int getKey() {
+		return this.key;
+	}
 
 }
