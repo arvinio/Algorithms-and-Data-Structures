e1d61fb8b60c4cf17910ca353d1809aa6336dfa3
Arvin Behshad <contact_arvin@me.com>
2015-12-03 19:38:08 +0100

    started BST class

--- src/binarySearchTree/Node.java
+++ src/binarySearchTree/Node.java
@@ -3,6 +3,7 @@ package binarySearchTree;
 public class Node {
 	private int key;
 	private String value;
+	private Node left, right;
 
 	public Node(int Key, String value) {
 		this.key = key;
@@ -11,6 +12,22 @@ public class Node {
 
 	public int getKey() {
 		return this.key;
+	}
+
+	public String getValue() {
+		return this.value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Node left() {
+		return this.left;
+	}
+
+	public Node right() {
+		return this.right;
 	}
 
 }
